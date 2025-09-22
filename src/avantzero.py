679234cbd03e45e016f60adc79f22c267646df4c
Jordy Veenstra <jordy.gaptx@gmail.com>
2025-09-22 22:53:52 +0200

    22/09 22:53
    Completion ofsplash screen design, wip remaining helper functions

--- src/avantzero.py
+++ src/avantzero.py
@@ -1,41 +1,25 @@
 from utils.SYSTEM import SYSTEM
-import utils.TEXTS as AVZTX
+from utils import TEXTS as AVZTX
 import time
 
 def main():
     try:
         AVANTZERO = SYSTEM()
-        AVANTZERO.show_splash(
-            initext=AVZTX.splashOne,
-            font_name="Helvetica",
-            size=48,
-            width=500,
-            height=500,
-            version="v0.2.0",
-        )
+        AVANTZERO.show_splash()
         time.sleep(2)
         AVANTZERO.update_splash(
-            text=AVZTX.splashOne,
-            font_name="Helvetica",
-            font_size=48,
             progress_val=33,
             status_text=AVZTX.splash_title_one
         )
         time.sleep(2)
         AVANTZERO.check_folder_structure()
         AVANTZERO.update_splash(
-            text=AVZTX.splashOne,
-            font_name="Helvetica",
-            font_size=48,
             progress_val=66,
             status_text=AVZTX.splash_title_two
         )
         time.sleep(2)
         AVANTZERO.check_dependencies()
         AVANTZERO.update_splash(
-            text=AVZTX.splashOne,
-            font_name="Helvetica",
-            font_size=48,
             progress_val=100,
             status_text=AVZTX.splash_title_three
         )
@@ -55,7 +39,7 @@ def main():
                 case 5:
                     AVANTZERO.exit_avantzero()
     except KeyboardInterrupt:
-        AVANTZERO.exit_avantzero()
+        AVANTZERO.exit_avantzero(0)
 
 if __name__ == "__main__":
     main()
--- src/utils/SYSTEM.py
+++ src/utils/SYSTEM.py
@@ -4,13 +4,14 @@ from utils.DEPGEN import DEPGEN
 from utils.PROJM import PROJM
 from utils.NLEGEN import NLEGEN
 from utils.UTILITIES import UTILS
-import utils.TEXTS as AVZTX
 import os, tkinter, time, sys
 from tkinter import font, ttk
+from PIL import Image, ImageTk
 
 class SYSTEM:
     COREDANLY, COREDATAGEN, COREDEPGEN, COREPROJM, CORENLEGEN, COREUTILS = DANLY, DATAGEN, DEPGEN, PROJM, NLEGEN, UTILS 
     
+    #DONE
     def __init__(self):
         self.splash_window = None
         self.splash_label = None
@@ -20,6 +21,7 @@ class SYSTEM:
         self.progress = None
         self.license_label = None
     
+    #DONE
     def center_window(self, window: str, width=500, height=500):
         screen_width = window.winfo_screenwidth()
         screen_height = window.winfo_screenheight()
@@ -27,74 +29,83 @@ class SYSTEM:
         y = int((screen_height / 2) - (height / 2))
         window.geometry(f"{width}x{height}+{x}+{y}") 
     
-    # ADD FIXES FOR SHOW SPLASH 
-    def show_splash(self, initext: str, font_name: str, size: int, width=500, height=500, version: str = "", copyright: str = ""):
+    #DONE
+    def show_splash(self, width=500, height=500):
         self.splash_window = tkinter.Tk()
         self.splash_window.overrideredirect(True)
         self.center_window(self.splash_window, width, height)
         self.splash_window.configure(bg="#2b2b2b")
-
-        # Logo / Title in center
-        splash_font = font.Font(family=font_name, size=size, weight="bold")
+        script_dir = os.path.dirname(os.path.abspath(__file__)) 
+        assets_path = os.path.join(script_dir, "..", "assets", "images", "APPOV_logo.png")
+        self.logo_image = Image.open(assets_path)
+        self.logo_image = self.logo_image.resize((42, 44))
+        self.logo_image = ImageTk.PhotoImage(self.logo_image)
+        self.logo_label = tkinter.Label(
+            self.splash_window,
+            image=self.logo_image,
+            bg="#2b2b2b"
+        )
+        self.logo_label.place(relx=0.0367, rely=0.0287, anchor="nw")
+        splash_logofont = font.Font(family='Quantum', size=175, weight="bold")
         self.splash_label = tkinter.Label(
             self.splash_window,
-            text=initext,
+            text='A',
             fg="white",
             bg="#2b2b2b",
-            font=splash_font
+            font=splash_logofont
         )
-        self.splash_label.pack(expand=True)
-
-        # Creator + copyright (stacked bottom-left)
-        self.creator_label = tkinter.Label(
+        self.splash_label.place(relx=0.5, rely=0.37, anchor="center")
+        splash_avztext = font.Font(family='Gantari ExtraLight', size=18)
+        text = "AVANTZERO"
+        spaced_text = "  ".join(text)
+        self.splash_logotitle = tkinter.Label(
             self.splash_window,
-            text="Created by A Pixelated Point of View",
-            fg="#fafafa",
+            text=spaced_text,
+            fg="white",
             bg="#2b2b2b",
-            font=font.Font(size=9)
+            font=splash_avztext
         )
-        self.creator_label.place(relx=0.01, rely=0.87, anchor="sw")
-
-        self.license_label = tkinter.Label(
-            self.splash_window,
-            text=copyright if copyright else "Licensed under MIT",
-            fg="#fafafa",
-            bg="#2b2b2b",
-            font=font.Font(size=9)
-        )
-        self.license_label.place(relx=0.01, rely=0.91, anchor="sw")
-
-        # Status text bottom-left
+        self.splash_logotitle.place(relx=0.494, rely=0.67, anchor="center")
+        splash_loadingtext = font.Font(family='Gantari Regular', size=10)
         self.status_label = tkinter.Label(
             self.splash_window,
             text="Starting...",
             fg="white",
             bg="#2b2b2b",
-            font=font.Font(size=10)
+            font=splash_loadingtext
         )
-        self.status_label.place(relx=0.01, rely=0.97, anchor="sw")
-
-        # Version bottom-right
+        self.status_label.place(relx=0.0367, rely=0.975, anchor="sw")
         self.version_label = tkinter.Label(
             self.splash_window,
-            text=version,
+            text='v0.2.0',
             fg="white",
             bg="#2b2b2b",
-            font=font.Font(size=10)
+            font=splash_loadingtext
         )
-        self.version_label.place(relx=0.99, rely=0.97, anchor="se")
-
-        # Progress bar just above bottom items
+        self.version_label.place(relx=0.967, rely=0.975, anchor="se")
+        style = ttk.Style(self.splash_window)
+        style.theme_use("clam")
+        style.configure(
+            "Custom.Horizontal.TProgressbar",
+            troughcolor="#2b2b2b",
+            background="#16ad8f", 
+            bordercolor="#383838",
+            lightcolor="#16ad8f",
+            darkcolor="#16ad8f",
+            thickness=10
+        )
         self.progress = ttk.Progressbar(
             self.splash_window,
             orient="horizontal",
             length=width - 40,
-            mode="determinate"
+            mode="determinate",
+            style="Custom.Horizontal.TProgressbar"
         )
-        self.progress.place(relx=0.5, rely=0.93, anchor="s")
-
+        self.progress.place(relx=0.5, rely=0.92, anchor="s")
         self.splash_window.update()
 
+
+    # WIP
     def check_folder_structure(self):
         try:
             if not os.path.exists(os.path.join(os.path.expanduser("~"), "Documents", "AvantZero")):
@@ -113,15 +124,15 @@ class SYSTEM:
     def check_dependencies(self):
         return True
     
-    def update_splash(self, text: str, font_name: str, font_size: int, progress_val: int, status_text=""):
+    # DONE
+    def update_splash(self, progress_val: int, status_text=""):
         if self.splash_window and self.splash_label:
-            splash_font = font.Font(family=font_name, size=font_size, weight="bold")
-            self.splash_label.config(text=text, font=splash_font)
             self.status_label.config(text=status_text)
             if self.progress:
                 self.progress["value"] = progress_val
             self.splash_window.update_idletasks()
 
+    # DONE
     def remove_splash(self):
         if self.splash_window:
             self.splash_window.destroy()
@@ -136,8 +147,13 @@ class SYSTEM:
     
     def main_menu(self):
         print("You made it to the main menu!")
+        time.sleep(5)
+        sys.exit(0)
         return False
 
+
+
+# LATER LMAO
     
     def boot_project_manager(self):
         print("Booting Project Manager...")
@@ -159,7 +175,6 @@ class SYSTEM:
         time.sleep(1)
         return True
     
-    def exit_avantzero(self, code):
+    def exit_avantzero(self, code: 0 | 1):
         print("Exiting AvantZero...")
-        time.sleep(2)
         sys.exit(code)
